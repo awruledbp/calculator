266d89c5811868c49da74f3395e957cafa3df2de
awruledbp <a.touran2@gmail.com>
2021-06-28 16:25:24 +0300

    Add event listener to append point (coma)

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 const currentInput = document.getElementById('current-input');
 const wholeExpression = document.getElementById('whole-expression');
 const digits = document.querySelectorAll('[class="button"]');
+const floating = document.querySelector('.coma.button');
 const MAX_INPUT_LENGTH = 20;
 
 let isCalculationFinished = false;
@@ -11,6 +12,11 @@ digits.forEach(button => {
   })
 });
 
+
+floating.addEventListener('click', _ => {
+  addPoint();
+});
+
 function addDigit(digit) {
   if (isCalculationFinished) {
     wholeExpression.innerText = '';
@@ -19,6 +25,14 @@ function addDigit(digit) {
   let inputLength = currentInput.innerText.length;
   if (inputLength < MAX_INPUT_LENGTH) {
     currentInput.innerText += digit;
+  }
+}
+
+function addPoint() {
+  const point = '.';
+  let isFloating = currentInput.innerText.includes(point);
+  if (!isFloating) {
+    currentInput.innerText += point;
   }
 }
 
