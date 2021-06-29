a76da335ae191f9d29c71a2ea5a7d3904a2b7db7
awruledbp <a.touran2@gmail.com>
2021-06-29 16:30:19 +0300

    Add calculate function to perform math ops

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,20 @@ const wholeExpression = document.getElementById('whole-expression');
 const digits = document.querySelectorAll('[class="button"]');
 const floating = document.querySelector('.coma.button');
 const negative = document.querySelector('.negative.button');
-const MAX_INPUT_LENGTH = 20;
 
+const ADDITION = '+';
+const SUBTRACTION = '-';
+const MULTIPLICATION = '*';
+const DIVISION = '÷';
+const KEYBOARD_DIVISION = '/';
+const RESULT = '=';
+const ALLCLEAR = 'AC';
+const DELETE = 'Del';
+const MAX_INPUT_LENGTH = 20;
+const WHOLE_EXP_MAX_LENGTH = 28;
+
+let currentOperator = '';
+let currentValue = 0;
 let isCalculationFinished = false;
 
 digits.forEach(button => {
@@ -48,6 +60,26 @@ function addMinus() {
     currentInput.innerText = minus + currentInput.innerText;
   } else {
     currentInput.innerText = currentInput.innerText.replace(minus, '');
+  }
+}
+
+
+function calculate() {
+  let operator = currentOperator;
+  let first = currentValue;
+  let second = Number.parseFloat(currentInput.innerText);
+
+  switch (operator) {
+    case ADDITION:
+      return first + second;
+    case SUBTRACTION:
+      return first - second;
+    case MULTIPLICATION:
+      return first * second;
+    case DIVISION:
+      return first / second;
+    default:
+      return 0;
   }
 }
 
